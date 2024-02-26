2a4c4f34a5edcb422cd73d4d058e10f789e96245
Robert Sullivan <rsullivan@unc.edu>
2024-02-26 16:49:32 +0100

    Update fastest/slowest to use util function

--- fastest_slowest.py
+++ fastest_slowest.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from datetime import timedelta
+from util import to_seconds
 
 # Load data from CSV file into DataFrame
 df = pd.read_csv('marathon-data.csv')
 
-# Convert 'split' and 'final' from these time strings to timedelta objects for comparison
-df['split_timedelta'] = pd.to_timedelta('0 days ' + df['split'])
-df['final_timedelta'] = pd.to_timedelta('0 days ' + df['final'])
+# Convert 'final' from these time strings to timedelta objects for comparison
+df['final_timedelta'] = df['final'].apply(to_seconds)
 
 # Find the row with the fastest final time
 fastest_final = df.loc[df['final_timedelta'].idxmin(), 'final']
