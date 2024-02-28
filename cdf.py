dc826345a24cdc5a581899d096eb4924b9ac97eb
Cole Vita <106690201+cole-vita-unc@users.noreply.github.com>
2024-02-28 14:24:07 +0100

    Update cdf to represent time in minutes

--- cdf.py
+++ cdf.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import numpy as np
-from util import to_seconds
+from util import to_minutes
 
 df = pd.read_csv('marathon-data.csv')
 
 # Step 1: Convert final times from HH:MM:SS to total seconds for easier computation
-df['final_seconds'] = df['final'].apply(to_seconds)
+df['final_minutes'] = df['final'].apply(to_minutes)
 
 # Step 2: Sort the final times in ascending order
-sorted_final_seconds = np.sort(df['final_seconds'])
+sorted_final_minutes = np.sort(df['final_minutes'])
 
 # Step 3: Calc CDF for each time
 # CDF value at each point = (num of data points below that point) / (the total number of points)
-cdf = np.arange(1, len(sorted_final_seconds)+1) / len(sorted_final_seconds)
+cdf = np.arange(1, len(sorted_final_minutes)+1) / len(sorted_final_minutes)
 
 # Plot the CDF
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(8, 6))
-plt.plot(sorted_final_seconds, cdf, marker='.', linestyle='none')
-plt.xlabel('Final Time in Seconds')
+plt.plot(sorted_final_minutes, cdf, marker='.', linestyle='none')
+plt.xlabel('Final Time in Minutes')
 plt.ylabel('CDF')
 plt.title('CDF of Marathon Final Times')
 plt.grid(True)
